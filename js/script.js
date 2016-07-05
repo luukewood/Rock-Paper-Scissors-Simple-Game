685432ba7c821b2c9fdb63fb69ad522609f64b56
Lukasz Dziedziola <dziedziola.lukasz@gmail.com>
2016-07-05 20:16:15 +0200

    Init repo and file structure

---
+++ js/script.js
@@ -0,0 +1,119 @@
+(function(){
+
+	var initButton = document.querySelector(".btn-succes");
+	var optionsField = document.querySelector(".game-options");
+	var buttonsOption = optionsField.querySelectorAll("button");
+	var textRound = document.querySelector(".game-output h3");
+	var player = {
+		name: '',
+		score: 0,
+		pick: ''
+	};
+	var computer = {
+		name: 'Computer',
+		score: 0,
+		pick: ''
+	};
+	var round = 0;
+
+	function newGame() {
+		
+		round += 1;
+		textRound.textContent = "Current Round" + " :" + round;
+		computer.score = 0;
+		computer.pick = "";
+		player.score = 0;
+		player.pick = "";
+		player.name = prompt("Give your name");
+		document.getElementById("playerName").textContent = player.name;
+		
+		for(i = 0; i < buttonsOption.length; i++) {
+			buttonsOption[i].removeAttribute("disabled");
+		};
+	};
+
+	function playerPick(pick) {
+		player.pick = pick;
+		console.log(player);
+		computerPick();
+		compareResults();
+	};
+
+	function computerPick() {
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+			computer.pick = "rock";
+			break;
+			case 1:
+			computer.pick = "paper";
+			break;
+			case 2:
+			computer.pick = "scissor";
+			break;
+		};
+	};
+
+	function compareResults() {
+
+		var gameResult;
+		if(player.pick === computer.pick) {
+			gameResult = "Remis!";
+		} else if ( player.pick === "scissor" && computer.pick === "rock") {
+			gameResult = "Computer wins!";
+			computer.score++;
+		} else if (player.pick === "rock" && computer.pick === "scissor") {
+			gameResult = "Computer wins!";
+			player.score++;
+		} else if(player.pick === "paper" && computer.pick === "rock") {
+			gameResult = "Player wins!";
+			player.score++;
+		} else if(player.pick === "rock" && computer.pick === "paper") {
+			gameResult = "Computer wins!";
+			computer.score++;
+		} else if(player.pick === "scissor" && computer.pick === "paper") {
+			gameResult = "Player wins!";
+			player.score++;
+		} else if(player.pick === "paper" && computer.pick === "scissor") {
+			gameResult = "Computer wins!";
+			computer.score++;
+		};
+
+		document.getElementById('computerPick').textContent = computer.pick;
+		document.querySelector("#playerPick").textContent = player.pick;
+		document.querySelector("#gameResult").innerHTML = gameResult;
+		document.querySelector("#playerScore").textContent = player.score;
+		document.querySelector("#computerScore").textContent = computer.score;
+
+		if(player.score >= 10) {
+			alert("Player wins whole round! Clicked 'Start New Game to start again'");
+			for(i = 0; i < buttonsOption.length; i++) {
+			buttonsOption[i].setAttribute("disabled" ,true);
+			};
+		} else if (computer.score >= 10) {
+			alert("Computer wins whole round! Click 'Start New Game' button to start again");
+			for(i = 0; i < buttonsOption.length; i++) {
+			buttonsOption[i].setAttribute("disabled" ,true);
+			};
+		};
+	};
+
+	initButton.addEventListener('click', newGame, false);
+
+	for(i = 0; i < buttonsOption.length; i++ ) {
+		buttonsOption[i].setAttribute("disabled", true);
+		buttonsOption[i].addEventListener('click', function(e){
+			target = e.currentTarget;
+			if ( target.classList.contains("button-rock")) {
+				var currentPick = 'rock';
+				playerPick(currentPick);
+			} else if(target.classList.contains("button-paper")) {
+				var currentPick = 'paper';
+				playerPick(currentPick);
+			} else {
+				var currentPick = 'scissor';
+				playerPick(currentPick);
+			};
+
+		}, false);
+	};
+})();
